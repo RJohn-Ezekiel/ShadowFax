b76ded662f920b422d1c07b10044ed76465dec41
R. John Ezekiel <r.johnezekiel3@gmail.com>
2026-03-12 20:29:54 +0530

    Update room.js

--- js/room.js
+++ js/room.js
@@ -1,96 +1,57 @@
 import { db } from "./firebase.js";
-import { ref, push, onChildAdded, set, remove, get, onDisconnect } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
-let username=localStorage.getItem("shadowfaxUser");
+import { ref,push,onChildAdded,remove }
+
+from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
+
+const chatBox=document.getElementById("chatBox");
+
+const sendBtn=document.getElementById("sendBtn");
+
+const clearBtn=document.getElementById("clearBtn");
+
+const msgInput=document.getElementById("msgInput");
+
+let user=localStorage.getItem("shadowfaxUser");
+
 let room=localStorage.getItem("shadowfaxRoom");
 
-const terminal=document.getElementById("terminal");
+document.getElementById("roomTitle").innerText="Room: "+room;
 
-function log(t){
-terminal.innerHTML+=t+"<br>";
-terminal.scrollTop=terminal.scrollHeight;
+sendBtn.onclick=function(){
+
+let msg=msgInput.value;
+
+push(ref(db,"messages/"+room),{
+
+user:user,
+
+msg:msg,
+
+time:new Date().toLocaleString()
+
+});
+
+msgInput.value="";
+
 }
 
-let userRef=ref(db,"rooms/"+room+"/users/"+username);
-
-set(userRef,true);
-
-onDisconnect(userRef).remove();
-
-push(ref(db,"rooms/"+room+"/messages"),{
-user:"system",
-text:username+" joined",
-time:Date.now()
-});
-
-window.addEventListener("beforeunload",function(){
-
-push(ref(db,"rooms/"+room+"/messages"),{
-user:"system",
-text:username+" left",
-time:Date.now()
-});
-
-});
-
-onChildAdded(ref(db,"rooms/"+room+"/messages"),data=>{
+onChildAdded(ref(db,"messages/"+room),data=>{
 
 let m=data.val();
 
-let d=new Date(m.time);
+let div=document.createElement("div");
 
-let ts=`[${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}]`;
+div.innerText=`[${m.time}] ${m.user}: ${m.msg}`;
 
-log(ts+" "+m.user+": "+m.text);
-
-});
-
-msg.addEventListener("keypress",function(e){
-
-if(e.key==="Enter") send();
+chatBox.appendChild(div);
 
 });
 
-function send(){
+clearBtn.onclick=function(){
 
-let text=msg.value;
+remove(ref(db,"messages/"+room));
 
-if(text.startsWith("/kick ")){
-
-let u=text.replace("/kick ","");
-
-remove(ref(db,"rooms/"+room+"/users/"+u));
-
-push(ref(db,"rooms/"+room+"/messages"),{
-user:"system",
-text:u+" kicked",
-time:Date.now()
-});
-
-}
-
-else{
-
-push(ref(db,"rooms/"+room+"/messages"),{
-user:username,
-text:text,
-time:Date.now()
-});
-
-}
-
-msg.value="";
-
-}
-
-document.getElementById("clearChatBtn").onclick=function(){
-
-remove(ref(db,"rooms/"+room+"/messages"));
-
-push(ref(db,"rooms/"+room+"/messages"),{
-user:"system",
-text:"chat cleared",
-time:Date.now()
-});
+chatBox.innerHTML="";
 
 }
