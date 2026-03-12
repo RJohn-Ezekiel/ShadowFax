a0755b0895e69cca77303c5f9d06a4a3d3f91c96
R. John Ezekiel <r.johnezekiel3@gmail.com>
2026-03-12 21:07:19 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/fireba
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC09ti0mlR9F9VIsN8zlqEQ6efY9R7PHX0",
-  authDomain: "shadowfax-1caa3.firebaseapp.com",
-  databaseURL: "https://shadowfax-1caa3-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "shadowfax-1caa3",
-  storageBucket: "shadowfax-1caa3.firebasestorage.app",
-  messagingSenderId: "399136805462",
-  appId: "1:399136805462:web:c60c057b277b16204221f7"
+apiKey: "AIzaSyC09ti0mlR9F9VIsN8zlqEQ6efY9R7PHX0",
+authDomain: "shadowfax-1caa3.firebaseapp.com",
+databaseURL: "https://shadowfax-1caa3-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "shadowfax-1caa3",
+storageBucket: "shadowfax-1caa3.firebasestorage.app",
+messagingSenderId: "399136805462",
+appId: "1:399136805462:web:c60c057b277b16204221f7"
 };
 
 const app = initializeApp(firebaseConfig);
