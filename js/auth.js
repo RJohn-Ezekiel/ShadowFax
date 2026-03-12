12c42d19574f24219b021f7a45d3647055af2518
R. John Ezekiel <r.johnezekiel3@gmail.com>
2026-03-12 20:18:41 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,36 +1,48 @@
-window.login = function(){
+import { db } from "./firebase.js";
+import { ref, get } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-database.js";
 
-let u = username.value.trim();
-let p = password.value.trim();
+const loginBtn = document.getElementById("loginBtn");
 
-get(ref(db,"users/"+u)).then(snap=>{
+loginBtn.onclick = function(){
 
-let data = snap.val();
+const username = document.getElementById("username").value.trim();
+const password = document.getElementById("password").value.trim();
+
+if(!username || !password){
+alert("enter username and password");
+return;
+}
+
+get(ref(db,"users/"+username)).then(snapshot=>{
+
+let data = snapshot.val();
 
 if(!data){
 alert("user not found");
 return;
 }
 
-if(data.password !== p){
+if(data.password !== password){
 alert("wrong password");
 return;
 }
 
-/* Save session */
+/* SAVE USER */
 
-localStorage.setItem("shadowfaxUser",u);
+localStorage.setItem("shadowfaxUser",username);
 
 /* ADMIN CHECK */
 
-if(u === "Admin" && p === "Eru_Iluvatar_031206"){
+if(username === "Admin" && password === "Eru_Iluvatar_031206"){
 localStorage.setItem("shadowfaxAdmin","true");
 }else{
 localStorage.setItem("shadowfaxAdmin","false");
 }
 
+/* GO TO DASHBOARD */
+
 window.location = "dashboard.html";
 
 });
 
-}
+};
