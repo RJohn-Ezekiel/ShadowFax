aac453b9692dca015f566605822c5f0a6b2ac31c
R. John Ezekiel <r.johnezekiel3@gmail.com>
2026-03-12 21:15:19 +0530

    Update matrix.js

--- js/matrix.js
+++ js/matrix.js
@@ -1,37 +1,53 @@
 const canvas = document.getElementById("matrixCanvas");
 const ctx = canvas.getContext("2d");
 
+/* FULLSCREEN CANVAS */
+
+function resizeCanvas(){
+
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+
+}
+
+resizeCanvas();
+
+window.addEventListener("resize",resizeCanvas);
+
+/* MATRIX CHARACTERS */
 
 const letters = "アァカサタナハマヤャラワ01ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const matrix = letters.split("");
 
 const fontSize = 16;
 
-const columns = canvas.width/fontSize;
+let columns = canvas.width / fontSize;
 
-const drops=[];
+let drops = [];
 
-for(let x=0;x<columns;x++)
-drops[x]=1;
+for(let x=0; x<columns; x++){
+drops[x] = 1;
+}
+
+/* DRAW LOOP */
 
 function draw(){
 
-ctx.fillStyle="rgba(0,0,0,0.05)";
+ctx.fillStyle = "rgba(0,0,0,0.05)";
 ctx.fillRect(0,0,canvas.width,canvas.height);
 
-ctx.fillStyle="#0F0";
-ctx.font=fontSize+"px monospace";
+ctx.fillStyle = "#00ff00";
+ctx.font = fontSize + "px monospace";
 
 for(let i=0;i<drops.length;i++){
 
-let text=matrix[Math.floor(Math.random()*matrix.length)];
+let text = matrix[Math.floor(Math.random()*matrix.length)];
 
 ctx.fillText(text,i*fontSize,drops[i]*fontSize);
 
-if(drops[i]*fontSize>canvas.height && Math.random()>0.975)
-drops[i]=0;
+if(drops[i]*fontSize > canvas.height && Math.random() > 0.975){
+drops[i] = 0;
+}
 
 drops[i]++;
 
